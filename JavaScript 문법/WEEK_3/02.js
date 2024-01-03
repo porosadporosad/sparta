155cd3e38cf4d598c86033120fcb4ba088e78b5d
porosadporosad <tmxk1594@naver.com>
2024-01-03 19:48:55 +0900

    3

---
+++ JavaScript 문법/WEEK_3/02.js
@@ -0,0 +1,20 @@
+var user = {
+    name : "wonjang",
+    gender:"male"
+};
+
+var changeName = function (user,newName){
+    return {
+        name : newName,
+        gender : user.gender
+    }
+}
+
+var user2 = changeName(user, "twojang");
+
+if(user !== user2){
+    console.log("유저 정보가 변경되었습니다");
+}
+
+console.log(user.name, user2.name);
+console.log(user === user2);
---
+++ JavaScript 문법/WEEK_3/05.js
@@ -0,0 +1,22 @@
+// var numbers = [10,20,3,16,45];
+
+// // var max = Math.max.apply(null,numbers);
+// // var min = Math.min.apply(null,numbers);
+
+// var max = Math.max(...numbers);
+// var min = Math.min(...numbers);
+
+
+// console.log(max,min);
+
+let a =3;
+let b = 5
+let c = 0;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);
